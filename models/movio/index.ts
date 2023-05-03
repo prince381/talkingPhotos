ba2cfb0b30ca070041e94b1ee25e0b9d675a6fb4
prince381 <powusu381@gmail.com>
2023-05-03 19:51:04 +0000

    fix: includes minor bug fixes

--- models/movio/index.ts
+++ models/movio/index.ts
@@ -148,7 +148,8 @@ class Movio {
     async addWatermarkToVideo(url: string, id: string, test: boolean) {
         // const URL = 'https://allinpod-watermarker.herokuapp.com/api/v1/add-watermark';
         // const URL = 'http://localhost:8080/api/v1/add-watermark';
-        const URL = 'https://allin-watermarker.herokuapp.com/api/v1/add-watermark';
+        // const URL = 'https://allin-watermarker.herokuapp.com/api/v1/add-watermark';
+        const URL = 'https://orca-app-d6v9p.ondigitalocean.app/api/v1/add-watermark'
 
         try {
             const { data: response } = await axios.post(URL, {
